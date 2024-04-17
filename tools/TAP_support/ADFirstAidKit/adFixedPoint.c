239aad6295461ba537129f56bccf0096f9beaa8b
Shreyas911 <shreyasg911@gmail.com>
2024-04-17 11:15:53 -0500

    Minor change: Dan's branch had some commented statements in adFixedPoint.c that have been removed. This is the latest version of ADFirstAidKit now, just like branch profiling_old.

--- tools/TAP_support/ADFirstAidKit/adFixedPoint.c
+++ tools/TAP_support/ADFirstAidKit/adFixedPoint.c
@@ -35,7 +35,6 @@ int adFixedPoint_notReduced(double cumul, float reduction) {
       ++(adjIters[fpDepth]) ;
       return 1 ;
     } else {
-/*       printf("%i adjoint iterations (reduced %e -> %e)\n", adjIters[fpDepth], refCumuls[fpDepth], cumul) ; */
       if (fpDepth<0) return 0 ; // protect from going out of bounds.
       --fpDepth ;
       return 0 ;
@@ -59,7 +58,6 @@ int adFixedPoint_tooLarge(double cumul, float minCumul) {
       ++(adjIters[fpDepth]) ;
       return 1 ;
     } else {
-/*       printf("%i adjoint iterations (residual %e < %e)\n", adjIters[fpDepth], cumul, minCumul) ; */
       if (fpDepth<0) return 0 ; // protect from going out of bounds.
       --fpDepth ;
       return 0 ;
